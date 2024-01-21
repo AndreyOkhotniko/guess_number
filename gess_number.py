4da7e43facea2c82c39c3afb2875882e32dcfb39
Okhotnikov <okhotnikoffan@yandex.ru>
2024-01-21 14:59:12 +0300

    паимпамп

---
+++ gess_number.py
@@ -0,0 +1,17 @@
+from random import randint 
+number=randint(1,100)
+print('Угадайте число от 1 до 100')
+
+while True:
+
+    guess=int(input('Введите число:'))
+
+    if guess<number:
+        print('Ваше число меньше')
+    if guess>number:
+        print('Ваше число больше')
+    if guess==number:
+
+        break
+
+print('отличная интуиция вы угадали')
